67c6873468ffadc2010b67167ee0d63f5b09ca04
Nishant <nishantchaurasia3035@gmail.com>
2026-06-03 20:46:10 +0530

    Delete 02_Video&Code directory

--- 02_Video&Code/03_StoreFreq.py
+++
@@ -1,6 +0,0 @@
-nums=[5,6,7,7,1,9,10,1,1,5]
-hash_map=dict()
-n=len(nums)
-for i in range(0,n):
-    hash_map[nums[i]]=hash_map.get(nums[i],0)+1
-print(hash_map)
--- 02_Video&Code/04_PalindromeRecursion.py
+++
@@ -1,7 +0,0 @@
-def palindrome(s,left,right):
-    if left >= right:
-        return True
-    if s[left]!=s[right]:
-        return False
-    return palindrome(left+1,right-1)
-    
