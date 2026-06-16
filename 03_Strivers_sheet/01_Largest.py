ac767a019c5949f105efc507fc850c2675213911
Nishant <nishantchaurasia3035@gmail.com>
2026-06-16 12:51:25 +0530

    Solved arrays easy lvl questions

---
+++ 03_Strivers_sheet/01_Largest.py
@@ -0,0 +1,14 @@
+nums = list(map(int,input("Enter nums: ").split()))
+
+n = len(nums)
+def largest(nums):
+    maximum = 0
+    for i in range(0,n):
+        if max > nums[i]:
+            i+=1
+        else:
+            maximum = nums [i]
+    return maximum
+
+print(maximum)
+
